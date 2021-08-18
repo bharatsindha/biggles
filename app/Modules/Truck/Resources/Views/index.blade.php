306ccbc57e4609a0dc42598a29e4df7861e5c26b
Bharat Sindha <sindhabharatsinh8@gmail.com>
2021-08-18 14:40:26 +0530

    - Setup the scss with laravel mix.

--- app/Modules/Truck/Resources/Views/index.blade.php
+++ app/Modules/Truck/Resources/Views/index.blade.php
@@ -75,8 +75,8 @@
                 // Buttons with Dropdown
                 buttons: [
                     {
-                        text: '{{ trans('common.add_new'). ' '. trans('common.truck') }}',
-                        className: 'add-new btn btn-primary mt-50',
+                        text: '{!! '<i class="ficon b-plus-icon" data-feather="plus"></i> &nbsp;'. trans('common.add_new'). ' '. trans('common.truck') !!} ',
+                        className: 'b-add-new btn btn-primary mt-50',
                         action: function (e, dt, node, config) {
                             window.location.href = "{{ route($moduleName.'.create') }}";
                         },
